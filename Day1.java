156616efadf306fb391da20391e01d10cfba3d3d
ainunijlal-code <ijlaltugas@gmail.com>
2024-09-02 18:23:52 +0800

    Hari 1

---
+++ Day1.java
@@ -0,0 +1,26 @@
+package javaapplication32;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Biodata diri :");
+
+        String nama = "Ainun Ijlal";
+        String nim = "D0223038";
+        String kelas = "Informatika E";
+        int angkatan = 2023;
+        int umur = 19;
+        char kelamin = 'L';
+        String alamat = "Majene";
+
+        System.out.println("Nama : " + nama);
+        System.out.println("Nim : " + nim);
+        System.out.println("kelas : " + kelas);
+        System.out.println("Angkatan : " + angkatan);
+        System.out.println("Umur : " + umur);
+        System.out.println("Kelamin : " + kelamin);
+        System.out.println("Alamat : " + alamat);
+
+    }
+}
