ad6c0f39746969fd5d86ae396584a6be7022b89e
ainunijlal-code <ijlaltugas@gmail.com>
2024-09-04 20:19:45 +0800

    Day3_tabel

---
+++ Day3.java
@@ -0,0 +1,18 @@
+package menti;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("+-----------------------+-------------+");
+        System.out.printf("| %-21s | %-11s |\n", "Nama", "NIM");
+        System.out.println("+-----------------------+-------------+");
+
+        System.out.println("| Ainun Ijlal           |  D0223038   |");
+        System.out.println("| rahmat                |  D0223039   |");
+        System.out.println("| aco iqbal             |  D0223040   |");
+        System.out.println("| rahmadi               |  D0223041   |");
+
+        System.out.println("+-----------------------+-------------+");
+    }
+}
