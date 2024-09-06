2124f2ce361d1a25f5d768fd1b740db941f5f129
ainunijlal-code <ijlaltugas@gmail.com>
2024-09-06 19:27:22 +0800

    Hari 5_byte

---
+++ Day5.java
@@ -0,0 +1,18 @@
+package menti;
+
+public class Day5 {
+
+    public static void main(String[] args) {
+
+        System.out.print("Perpustakaan : \n");
+
+        byte totalBuku = 50; // Total buku di perpustakaan
+
+        System.out.printf("Bku di perpustakaan : %d \n", totalBuku);
+
+        byte bukuDipinjam = 12; // Jumlah buku yang dipinjam
+
+        // Output hasil
+        System.out.println("Buku yang dipinjam : " + bukuDipinjam);
+    }
+}
