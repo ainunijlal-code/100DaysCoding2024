6f09c0d39d1bfe3097f2838c4608e5cd2760cb18
ainunijlal-code <ijlaltugas@gmail.com>
2024-09-07 11:11:05 +0800

    Hari 6_short

---
+++ Day6.java
@@ -0,0 +1,22 @@
+package menti;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ketinggian tempat : ");
+
+        // Untuk Deklarasi variabel dengan tipe data short
+        short tinggi;
+
+        // Untuk Inisialisasi variabel atau memberikan nilai awal
+        tinggi = 30;
+        System.out.print("Ketinggian awal : ");
+        System.out.println(tinggi + " meter");
+
+        // Untuk Update nilai variabel
+        tinggi = 45;
+        System.out.print("Ketinggian setelah pendakian : ");
+        System.out.println(tinggi + " meter");
+    }
+}
