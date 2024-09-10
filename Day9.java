51d72d493549e5dd8c395900a0eb8b8aef80452b
ainunijlal-code <ijlaltugas@gmail.com>
2024-09-10 19:19:52 +0800

    Hari 9

---
+++ Day9.java
@@ -0,0 +1,24 @@
+package menti;
+
+public class Day9 {
+
+    public static void main(String[] args) {
+
+        byte jumlahKaktus;
+        short jumlahMawar;
+        int totalTanaman;
+
+        jumlahKaktus = 7;
+        jumlahMawar = 15;
+        totalTanaman = 22;
+
+        System.out.println("*******************************");
+        System.out.println("*   KOLEKSI TANAMAN RUMAH    *");
+        System.out.println("*******************************");
+        System.out.println(" Koleksi Kaktus    : " + jumlahKaktus + " pot");
+        System.out.println("-------------------------------");
+        System.out.println(" Koleksi Mawar     : " + jumlahMawar + " pot");
+        System.out.println("-------------------------------");
+        System.out.println(" Total Tanaman     : " + totalTanaman + " pot");
+    }
+}
