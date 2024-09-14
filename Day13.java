6d447582b8bc882c09e951ce7d071f717a78c3f0
ainunijlal-code <ijlaltugas@gmail.com>
2024-09-14 20:56:35 +0800

    Hari 13

---
+++ Day13.java
@@ -0,0 +1,54 @@
+package menti2;
+
+public class Day13 {
+
+    public static void main(String[] args) {
+
+        // Tipe Data Primitif
+        byte angka1 = 10;
+        short angka2 = 20;
+        int angka3 = 30;
+        long angka4 = 40L;
+
+        float angka5 = 50.5f;
+        double angka6 = 30.90;
+        boolean nilai7 = true;
+        char karakter8 = 'A';
+
+        // Tipe Data Referensi
+        Byte angkaByte = 100;
+        Short angkaShort = 200;
+        Integer angkaInt = 300;
+        Long angkaLong = 400L;
+        Float angkaFloat = 10.5f;
+        Double angkaDouble = 20.90;
+        String teks = "Belajar Coding Java";
+        Boolean nilai = true;
+        Character karakter = 'B';
+
+        // Menampilkan hasil
+        System.out.println("Tipe Data Primitif:");
+        System.out.println("byte: " + angka1);
+        System.out.println("short: " + angka2);
+        System.out.println("int: " + angka3);
+        System.out.println("long: " + angka4);
+        System.out.println("float: " + angka5);
+        System.out.println("double: " + angka6);
+        System.out.println("boolean: " + nilai7);
+        System.out.println("char: " + karakter8);
+
+
+        // Menampilkan hasil
+        System.out.println("\nTipe Data Referensi :");
+        System.out.println("Byte: " + angkaByte);
+        System.out.println("Short: " + angkaShort);
+        System.out.println("Integer: " + angkaInt);
+        System.out.println("Long: " + angkaLong);
+        System.out.println("Float: " + angkaFloat);
+        System.out.println("Double: " + angkaDouble);
+        System.out.println("Boolean: " + nilai);
+        System.out.println("Character: " + karakter);
+        System.out.println("String: " + teks);
+
+    }
+}
