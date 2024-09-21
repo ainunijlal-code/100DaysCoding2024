7e7a01ea88f1da1ea837e0c20b5b025ff193b3d9
ainunijlal-code <ijlaltugas@gmail.com>
2024-09-21 20:26:05 +0800

    Hari 20

---
+++ Day20.java
@@ -0,0 +1,14 @@
+package menti2;
+
+public class Day20 {
+
+    public static void main(String[] args) {
+
+        Byte angka = 20;// Mendeklarasikan dan menginisialisasi objek Byte
+
+        System.out.println("nilai Byte : " + angka);// Mencetak nilai objek Byte
+
+        int tukar = angka.intValue();// Menggunakan metode untuk mengubah nilai objek Byte ke tipe int
+        System.out.println("Di ubah menjadi tipe data int : " + tukar);
+    }
+}
