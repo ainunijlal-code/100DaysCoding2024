b75b6bb90db0a69d5c60e577e5febf5d555b2b12
ainunijlal-code <ijlaltugas@gmail.com>
2024-09-24 20:30:33 +0800

    Hari 23

---
+++ Day23.java
@@ -0,0 +1,21 @@
+package menti3;
+
+public class Day23 {
+
+    public static void main(String[] args) {
+
+        final Integer angka;
+        Long angka2;
+
+        angka = 20;
+        angka2 = 40L;
+
+        System.out.println("hasil final : " + angka);
+        System.out.printf("hasil : %d \n", angka2);
+
+        angka2 += 4;
+
+        System.out.println("hasil update : " + angka2);
+
+    }
+}
