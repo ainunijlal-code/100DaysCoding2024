82aab4f2655df675f9b416490aab557e45499af1
ainunijlal-code <ijlaltugas@gmail.com>
2024-09-25 21:50:19 +0800

    Hari 24

---
+++ Day24.java
@@ -0,0 +1,21 @@
+package menti3;
+
+public class Day24 {
+
+    public static void main(String[] args) {
+
+        Float angka;
+
+        angka = 23.5f;
+
+        System.out.println("nilai : " + angka);
+
+        angka *= 5;
+        System.out.println("hasil update : " + angka);
+        angka -= 5;
+        System.out.println("hasil update : " + angka);
+        angka += 5;
+        System.out.println("hasil update : " + angka);
+
+    }
+}
