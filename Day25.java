95b76ea62a91ec681d11b24b38bca20776abae69
ainunijlal-code <ijlaltugas@gmail.com>
2024-09-26 22:20:11 +0800

    Hari 25

---
+++ Day25.java
@@ -0,0 +1,19 @@
+package menti3;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+
+        Double angka1;
+        Double angka2;
+        Double angka3;
+
+        angka1 = 10.5;
+        angka2 = 10.324;
+        angka3 = 10.587;
+
+        System.out.printf("hasil :%.1f \n", angka1);
+        System.out.printf("hasil :%.2f \n", angka2);
+        System.out.printf("hasil :%.3f \n", angka3);
+    }
+}
