a37578ce099d845766e5091c1455b3f177e1a939
ainunijlal-code <ijlaltugas@gmail.com>
2024-10-03 20:05:50 +0800

    Hari 32

---
+++ Day32.java
@@ -0,0 +1,23 @@
+
+package menti4;
+
+
+public class Day32 {
+    public static void main(String[] args) {
+        
+        int angka = 11;
+        double angka2 = 30.5;
+        boolean nilai = true;
+        
+        angka += 2;
+        angka2 -= 3;
+        
+        String ubah1 = Integer.toString(angka);
+        String ubah2 = Double.toString(angka2);
+        String ubah3 = Boolean.toString(nilai);
+        
+        System.out.println(ubah1);
+        System.out.println(ubah2);
+        System.out.println(ubah3);
+    }
+}
