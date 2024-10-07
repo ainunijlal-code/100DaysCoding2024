3b22a6076a2fe57814f5c02ccf2c2f0ad0dbfd9f
ainunijlal-code <ijlaltugas@gmail.com>
2024-10-07 20:03:16 +0800

    Hari 36

---
+++ Day36.java
@@ -0,0 +1,17 @@
+package menti4;
+
+public class Day36 {
+
+    public static void main(String[] args) {
+
+        int angka1 = 10;
+        int angka2 = 20;
+        int angka3 = 30;
+
+        int aritmatika = (angka1 + angka3) / angka2;
+        System.out.println(aritmatika);
+
+        int kali = aritmatika * angka1;
+        System.out.println(kali);
+    }
+}
