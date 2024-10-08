ef6f0a66c2052f75ee03dbcd96d1e7ebe216ed9e
ainunijlal-code <ijlaltugas@gmail.com>
2024-10-08 19:42:05 +0800

    Hari 37

---
+++ Day37.java
@@ -0,0 +1,21 @@
+
+package menti4;
+
+
+public class Day37 {
+    public static void main(String[] args) {
+        
+        int angka = 30;
+        System.out.println("nilai awal : "+ angka);
+        
+        angka += 10;
+        System.out.println("Setelah penambahan 10 : "+ angka);
+        
+        angka -= 5;
+        System.out.println("Setelah penambahan 5 : "+ angka);
+        
+        angka = 50;
+        System.out.printf("hasil update : %d\n",angka);
+            
+    }
+}
