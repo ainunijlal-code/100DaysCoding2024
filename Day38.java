02f2e6984e862dd8d55f14a2117f1ef2e4938ddf
ainunijlal-code <ijlaltugas@gmail.com>
2024-10-09 19:51:10 +0800

    Hari 38

---
+++ Day38.java
@@ -0,0 +1,18 @@
+
+package menti4;
+
+
+public class Day38 {
+    public static void main(String[] args) {
+        
+         int angka = 50;
+       
+         angka *= 3;
+         angka /= 2;
+         System.out.println("Hasil operator penugasan kali dan bagi : "+angka);
+         
+         String ubah = Integer.toString(angka);
+         System.out.println("Setelah di ubah ke String : "+ubah);
+                 
+    }
+}
