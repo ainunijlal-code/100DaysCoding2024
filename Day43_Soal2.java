074b1f481df7847268d42fcb4d4c655f10fcb702
ainunijlal-code <ijlaltugas@gmail.com>
2024-10-14 18:12:53 +0800

    Day 43

---
+++ Day43_Soal2.java
@@ -0,0 +1,22 @@
+package menti5;
+
+public class Soal2 {
+
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 20;
+
+        System.out.println(b);
+        b -= a;
+        System.out.println(b);
+        b *= a;
+        System.out.println(b);
+        b -= 15;
+        System.out.println(b);
+        b /= 15;
+        System.out.println(b);
+        b += 15;
+        System.out.println(b);
+    }
+}
