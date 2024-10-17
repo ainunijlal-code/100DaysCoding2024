25d902ba8f18b22be3dcb79f01587862f7687793
ainunijlal-code <ijlaltugas@gmail.com>
2024-10-17 20:33:56 +0800

    Hari 46

---
+++ Day46.java
@@ -0,0 +1,22 @@
+package menti5;
+
+public class Day46 {
+
+    public static void main(String[] args) {
+
+        boolean benar = true;
+        boolean salah = false;
+
+        boolean not = !benar;
+        boolean not2 = !salah;
+
+        System.out.println("Negasi dari true adalah : " + not);
+        System.out.println("Negasi dari false adalah : " + not2);
+        System.out.printf("nilai dari boolean sebelum di negasi :\n%b\n%b\n",!not, !not2);
+      
+        
+        
+        
+
+    }
+}
