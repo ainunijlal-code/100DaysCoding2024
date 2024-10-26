f4d103201bfe701702d1f73446816e145cddea79
ainunijlal-code <ijlaltugas@gmail.com>
2024-10-26 21:43:07 +0800

    Hari 55

---
+++ Day55.java
@@ -0,0 +1,16 @@
+package menti;
+
+public class Main {
+	public static void main(String[] args) {	
+		int angka = 10;
+		
+		if(angka < 50){
+		     for (int i = 5; i < angka; i++) {
+             System.out.println(i);
+		}
+		}else{
+		     System.out.println(angka);
+		}
+			 
+	}
+}
