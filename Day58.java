36bb541c1709e10e27f473df6986e04804f8a681
ainunijlal-code <ijlaltugas@gmail.com>
2024-10-29 20:28:34 +0800

    Hari 58

---
+++ Day58.java
@@ -0,0 +1,16 @@
+package menti6;
+
+public class Day58 {
+
+    public static void main(String[] args) {
+
+        int angka = 100;
+
+        while (angka > 30 && angka > 50) {
+
+            System.out.println(angka);
+            angka--;
+
+        }
+    }
+}
