f490c11efe657d2bdc1645912df4dc544f01b9e0
ainunijlal-code <ijlaltugas@gmail.com>
2024-11-07 20:59:08 +0800

    Hari 67

---
+++ Day67.java
@@ -0,0 +1,17 @@
+package menti7;
+
+public class Day67 {
+
+    public void panggil() {
+        System.out.println("Sekarang Day 67");
+
+    }
+
+    public static void main(String[] args) {
+
+        Day67 teks = new Day67();
+
+        teks.panggil();
+    }
+
+}
