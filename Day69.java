b8465e42131e61a47ae3536e847af352339043d4
ainunijlal-code <ijlaltugas@gmail.com>
2024-11-09 21:08:11 +0800

    Hari 69

---
+++ Day69.java
@@ -0,0 +1,20 @@
+
+package menti7;
+
+public class Day69 {
+    
+    
+    public void cetak(String kalimat){
+        
+        System.out.println(kalimat);
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        Day69 kata = new Day69();
+        
+        kata.cetak("Sekarang Day 69");
+    }
+ 
+}
