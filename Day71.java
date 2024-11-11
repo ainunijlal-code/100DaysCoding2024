bf2a05fa9169f66a1f34ab21b8efdffdc9dbb037
ainunijlal-code <ijlaltugas@gmail.com>
2024-11-11 19:56:31 +0800

    Hari 71

---
+++ Day71.java
@@ -0,0 +1,21 @@
+
+package menti8;
+
+
+public class Day71 {
+    
+    
+    public static void panggil(String hari){
+        System.out.print(hari);
+    }
+    
+    public static int tanggal(int a){
+        System.out.println(a);
+        return a;
+    }
+    public static void main(String[] args) {
+        
+        panggil("Sekarang Day ke-");
+        tanggal(71);
+    }
+}
