81ba16b71a45b80985a5cff1034d8b33067716af
ainunijlal-code <ijlaltugas@gmail.com>
2024-11-13 20:36:58 +0800

    Hari 73

---
+++ Day73.java
@@ -0,0 +1,14 @@
+
+package menti8;
+
+public class Day73 {
+    public static void main(String[] args) {
+        
+        String nama = "Ainun Ijlal";
+        
+        char pertama = nama.charAt(0);
+        char kedua = nama.charAt(6);
+        
+        System.out.println(String.valueOf(pertama) + String.valueOf(kedua));
+    }
+}
