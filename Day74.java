c470b9dfd41d3bfc253e8edaf94a733869795566
ainunijlal-code <ijlaltugas@gmail.com>
2024-11-14 20:29:23 +0800

    Hari 74

---
+++ Day74.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		String kata = "day 74";
+		
+		int karakter = kata.length();
+		
+		System.out.println("panjang karakter = " + karakter);
+	}
+}
