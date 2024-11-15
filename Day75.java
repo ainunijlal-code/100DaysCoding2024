1aa0b8250b59bd6bfca1cc515cec6266fd649059
ainunijlal-code <ijlaltugas@gmail.com>
2024-11-15 18:50:11 +0800

    Hari 75

---
+++ Day75.java
@@ -0,0 +1,17 @@
+
+package menti8;
+
+
+public class Day75 {
+    public static void main(String[] args) {
+        
+        
+        String nama = "AINUN IJLAL";
+        
+        int jumlah = nama.length();
+        String ubah = nama.toLowerCase();
+        
+        System.out.println("jumlah karakter : " + jumlah);
+        System.out.println("ubah ke huruf kecil : " + ubah);
+    }
+}
