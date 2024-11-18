a1a40c720bdc703273967e1bc0f0f57bb3ac336b
ainunijlal-code <ijlaltugas@gmail.com>
2024-11-18 22:27:54 +0800

    Hari 78

---
+++ Day78.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		String hari = "DAY 78";
+		String hari2 = "day 78";
+	
+	String ubah = hari.toLowerCase();		
+	String ubah2 = hari2.toUpperCase();
+	
+	System.out.print("Apakah DAY 78 = day 78 : ");
+	System.out.println(ubah.equalsIgnoreCase(ubah2));
+		
+		
+	}
+}
