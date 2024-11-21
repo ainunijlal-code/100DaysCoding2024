68a4e9931e65be4f09ef9e95c44b3904c080d0e5
ainunijlal-code <ijlaltugas@gmail.com>
2024-11-21 20:56:16 +0800

    Hari 81

---
+++ Day81.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		int angka = 10;
+		int angka2 = 10;
+		
+		String ubah = String.valueOf(angka);
+		String ubah2 = String.valueOf(angka2);
+		
+		System.out.println(ubah.equals(ubah2));
+	}
+}
