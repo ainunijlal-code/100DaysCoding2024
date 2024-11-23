223d4b549d389f2a1dfc8cccb3543d0f7c3a0c37
ainunijlal-code <ijlaltugas@gmail.com>
2024-11-23 21:22:40 +0800

    Hari 83

---
+++ Day83.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+
+        int [] array = new int[5];
+         
+        System.out.println("Isi array1 setelah deklarasi dan inisialisasi:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("array1[" + i + "]: " + array[i]);
+        }
+	}
+}
