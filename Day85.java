0af8ad5baaec5acc1d807f14c3e2c4c939606bf4
ainunijlal-code <ijlaltugas@gmail.com>
2024-11-25 21:31:59 +0800

    Hari 85

---
+++ Day85.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		
+	
+		 int[] angka1 = {1, 2, 3};
+		 int[] angka2 = {1, 2, 3, 4};
+		 int[] angka3 = {1, 2, 3, 4, 5};
+		 String[] kata = {"java", "day85"};
+
+         
+	}
+}
