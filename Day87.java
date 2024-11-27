79567b82764593933be32ab4124ca172bdf45645
ainunijlal-code <ijlaltugas@gmail.com>
2024-11-27 20:21:55 +0800

    Hari 87

---
+++ Day87.java
@@ -0,0 +1,17 @@
+
+package menti9;
+
+public class Day87 {
+    public static void main(String[] args) {
+        
+        
+        int[] angka = {1,2,3,4};
+        
+        int indeks = 3;
+        angka[indeks] = 10;
+        
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println(angka[i]);
+        }
+    }
+}
